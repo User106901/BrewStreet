5aa5cb07485578a1e209be268c602cd35808ce84
andiRrapaj <andirrapaj4@gmail.com>
2025-02-21 10:40:04 +0100

    rezervimet

--- aa/BrewStreet/src/rezervime/rezervimeloged.java
+++ aa/BrewStreet/src/rezervime/rezervimeloged.java
@@ -18,7 +18,7 @@ public class rezervimeloged {
     private JTextField textField_1;
     private JTextField textField_3;
     private JTextField textField_2;
-    private JTable table;
+    private JTable tableRes;
 
 
     public rezervimeloged(JPanel container, CardLayout cards) {
@@ -43,76 +43,114 @@ public class rezervimeloged {
         
         panel.add(btnNewButton);
         
-//        textField_3 = new JTextField();
-//        textField_3.setColumns(10);
-//        textField_3.setBounds(152, 241, 158, 32);
-//        panel.add(textField_3);
-//        
-//        
-//        JDateChooser dateChooser = new JDateChooser();
-//        panel.add(dateChooser);
-//
-//        panel.setLayout(null);
-//        dateChooser.setBounds(152, 147, 158, 32);
-//        
-//        textField_2 = new JTextField();
-//        textField_2.setColumns(10);
-//        textField_2.setBounds(561, 147, 158, 32);
-//        panel.add(textField_2);
-//        
-//        JLabel lblNewLabel = new JLabel("Data");
-//        lblNewLabel.setFont(new Font("Sitka Small", Font.PLAIN, 18));
-//        lblNewLabel.setBounds(42, 147, 100, 32);
-//        panel.add(lblNewLabel);
-//        
-//        JLabel lblEmri = new JLabel("Emri");
-//        lblEmri.setFont(new Font("Sitka Small", Font.PLAIN, 18));
-//        lblEmri.setBounds(42, 241, 100, 32);
-//        panel.add(lblEmri);
-//        
-//        JLabel lblDetaje = new JLabel("Detaje");
-//        lblDetaje.setFont(new Font("Sitka Small", Font.PLAIN, 18));
-//        lblDetaje.setBounds(455, 241, 69, 32);
-//        panel.add(lblDetaje);
-//        
-//        JLabel lblNumriIPersonave = new JLabel("Nr. Personave");
-//        lblNumriIPersonave.setFont(new Font("Sitka Small", Font.PLAIN, 18));
-//        lblNumriIPersonave.setBounds(385, 147, 139, 32);
-//        panel.add(lblNumriIPersonave);
-//        
+        
+      //--------------------------------------------------------------------------------------------------------------------------------------------  
+
+        JFrame fr = new JFrame();
+		fr.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		fr.setTitle("Shto Rezervim");
+		fr.setLocation(400, 150);
+		fr.setSize(600,500);
+		fr.getContentPane().setLayout(null);
+		
+		
+		
+		
+      JDateChooser dataAdd = new JDateChooser();
+      dataAdd.setBounds(99, 159, 167, 28);
+      fr.getContentPane().add(dataAdd);
+      
+      JTextField emriAdd = new JTextField();
+      emriAdd.setBounds(99, 68, 167, 34);
+      fr.getContentPane().add(emriAdd);
+      emriAdd.setColumns(10);
+      
+      JTextField telAdd = new JTextField();
+      telAdd.setColumns(10);
+      telAdd.setBounds(400, 68, 176, 34);
+      fr.getContentPane().add(telAdd);
+      
+      JTextArea detajeAdd = new JTextArea();
+      detajeAdd.setLineWrap(true);
+      detajeAdd.setWrapStyleWord(true);
+
+      JScrollPane scrollPane = new JScrollPane(detajeAdd);
+      scrollPane.setBounds(409, 159, 167, 173);
+
+      fr.getContentPane().add(scrollPane);
+
+      
+      JLabel lblNewLabel = new JLabel("Emri");
+      lblNewLabel.setFont(new Font("Times New Roman", Font.ITALIC, 21));
+      lblNewLabel.setBounds(25, 68, 64, 34);
+      fr.getContentPane().add(lblNewLabel);
+      
+      JLabel lblData = new JLabel("Data");
+      lblData.setFont(new Font("Times New Roman", Font.ITALIC, 21));
+      lblData.setBounds(25, 159, 64, 34);
+      fr.getContentPane().add(lblData);
+      
+      JLabel lblNrTel = new JLabel("Nr. Tel");
+      lblNrTel.setFont(new Font("Times New Roman", Font.ITALIC, 21));
+      lblNrTel.setBounds(311, 68, 79, 34);
+      fr.getContentPane().add(lblNrTel);
+      
+      JLabel lblDetaje = new JLabel("Detaje");
+      lblDetaje.setFont(new Font("Times New Roman", Font.ITALIC, 21));
+      lblDetaje.setBounds(335, 159, 64, 34);
+      fr.getContentPane().add(lblDetaje);
+      
+      JButton applyBtn = new JButton("Apply");
+      applyBtn.addActionListener(new ActionListener() {
+      	public void actionPerformed(ActionEvent e) {
+      		
+      	}
+      });
+      applyBtn.setBackground(Color.WHITE);
+      applyBtn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      applyBtn.setBounds(85, 380, 121, 34);
+      fr.getContentPane().add(applyBtn);
+      
+      JButton btnCancel = new JButton("Cancel");
+      btnCancel.setBackground(Color.WHITE);
+      btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+      btnCancel.setBounds(356, 380, 121, 34);
+      fr.getContentPane().add(btnCancel); 
+      
+      
+      
         JButton btnNewButton_1 = new JButton("Shto");
+        btnNewButton_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		 fr.setVisible(true);
+        	}
+        });
+        btnNewButton_1.setBackground(Color.WHITE);
         btnNewButton_1.setBounds(757, 205, 120, 109);
         panel.add(btnNewButton_1);
         JButton btnNewButton_2 = new JButton("New button");
         btnNewButton_2.setBounds(637, 716, 22, 21);
         panel.add(btnNewButton_2);
         
-        table = new JTable();
-        table.setBounds(44, 91, 615, 646);
-        panel.add(table);
+        tableRes = new JTable();
+        tableRes.setBounds(44, 91, 615, 646);
+        panel.add(tableRes);
+        
        
-        JFrame miniFrame = new JFrame();
-        miniFrame.setSize(200,200);
-        miniFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        miniFrame.setLocation(250,250);
         
         
        
         JButton btnNewButton_1_1 = new JButton("Shiko Rezervimin");
+        btnNewButton_1_1.setBackground(Color.WHITE);
         btnNewButton_1_1.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		
-                miniFrame.setVisible(true);
+               
         	}
         });
         btnNewButton_1_1.setBounds(757, 434, 120, 109);
         panel.add(btnNewButton_1_1);
         
-//        JTextArea textArea = new JTextArea();
-//        textArea.setText("Enter Discription... ");
-//        textArea.setBounds(561, 244, 272, 97);
-//               panel.add(textArea);
-//       
 
         
     }
